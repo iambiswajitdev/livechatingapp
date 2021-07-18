def988b69d119328fe1db64ef0d11395d6390fa9
mrbiswajit-skypoint <mrbiswajitmondal24@gmail.com>
2021-07-18 12:52:29 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import "./assets/css/style.css";
+import Chat from "./components/Chat";
+import SignIn from "./components/SignIn";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <>
+      <div className="main">{user ? <Chat /> : <SignIn />}</div>
+    </>
+  );
+}
+
+export default App;
